a942f456ca32627227373400e7a267e70f2d3f95
FantasyBaby <625353861@qq.com>
2015-07-04 23:59:17 +0800

    add the linear_list algorithm

---
+++ data_structure/src/com/fantasybaby/entity/SoryEntity.java
@@ -0,0 +1,14 @@
+package com.fantasybaby.entity;
+
+import java.io.Serializable;
+
+public class SoryEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1373869009287460257L;
+
+	
+
+}
