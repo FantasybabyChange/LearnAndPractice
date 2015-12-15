380979ea4d9049dec574d905d18fa58564f1b51c
FantasyBaby <625353861@qq.com>
2015-12-15 23:30:56 +0800

    add a new site

--- jvm-test/src/main/java/com/fantasybaby/bytecode/LocalVariable.java
+++ jvm-test/src/main/java/com/fantasybaby/bytecode/LocalVariable.java
@@ -1,5 +1,6 @@
 package com.fantasybaby.bytecode;
 //http://www.importnew.com/17086.html
+//http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html
 public class LocalVariable {
 	private int b = 1001;
 	/*public static void main(String[] args) {
