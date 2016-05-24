b73c4766710926953f1da01e8c63b8b579ad1f54
fantasybaby <625353861@qq.com>
2016-05-24 17:14:42 +0800

    add test lockfile

---
+++ daliy_test/src/main/java/com/fantasybaby/lockfile/LocalFileJava.java
@@ -0,0 +1,9 @@
+package com.fantasybaby.lockfile;
+
+import java.io.File;
+
+public class LocalFileJava {
+	public static void main(String[] args) {
+		System.out.println(File.separator+" "+File.separatorChar+" "+File.pathSeparator+" "+File.pathSeparatorChar);
+	}
+}
