3f315d2d3d4b55577420b221ea554ba5ab2214c0
fantasybaby <625353861@qq.com>
2016-07-20 16:23:35 +0800

    add a new style for alert window

---
+++ daliy_test/src/main/java/com/fantasybaby/rcp/AlertMessageDialog.java
@@ -0,0 +1,271 @@
+package com.fantasybaby.rcp;
+
+import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Layout;
+import org.eclipse.swt.widgets.Shell;
+/**
+ * A alert message Dialog in a new shell
+ * @author FantasyBaby
+ *
+ */
+public class AlertMessageDialog {
+//	private static Display display = new Display();
+    private  Shell shell;
+    private Label fisrtLabel;
+    private Label secondLabel;
+    private static AlertMessageDialog _instance;
+    private Layout shellLayout;
+    private  String alertInfo = "这是提示!!!";
+    private boolean isAutoClose = false;
+    private int closeSeconds = 3; 
+    private int shellWidth = 100;
+    private int shellHeight = 100;
+    private int shellLocationx = 0;
+    private int shellLocationy = 0;
+    private boolean isMove = false;
+    private int moveSpeed = 3;
+    private int alpha = 150;
+    private ShellPositionEnum position = null;
+    
+    private Color shellBackColor = SWTResourceManager.getColor(SWT.COLOR_RED);
+    private Font labelFont = SWTResourceManager.getFont("Microsoft YaHei UI", StationUIUtils.scale(15), SWT.BOLD);
+    private Color labelBackColor = SWTResourceManager.getColor(SWT.COLOR_RED);
+    private Color labelColor = SWTResourceManager.getColor(SWT.COLOR_BLACK);
+	private AlertMessageDialog() {
+		
+	}
+	public void init(String alertInfo,Shell parentShell){
+		this.alertInfo = alertInfo;
+		if(parentShell != null){
+			shell = new Shell(parentShell,SWT.NONE);
+		}else{
+			shell = new Shell(new Display());
+		}
+	}
+	public void init(String alertInfo,Shell parentShell,boolean isAutoClose,int closeSeconds){
+		this.isAutoClose = isAutoClose;
+		if(closeSeconds > 0 ){
+			this.closeSeconds = closeSeconds;
+		}
+		init(alertInfo,parentShell);
+	}
+	public void init(String alertInfo,Shell parentShell,int shellWidth,int shellHeight,ShellPositionEnum position){
+		if(shellWidth > 0 && shellHeight > 0){
+			this.shellWidth = shellWidth;
+			this.shellHeight = shellHeight;
+		}
+		this.position = position;
+		init(alertInfo, parentShell);
+	}
+	public void init(String alertInfo,Shell parentShell,int shellWidth,int shellHeight,int shellLocationx,int shellLocationy){
+		if(shellWidth > 0 && shellHeight > 0){
+			this.shellWidth = shellWidth;
+			this.shellHeight = shellHeight;
+		}
+		this.shellLocationx = shellLocationx;
+		this.shellLocationy = shellLocationy;
+		init(alertInfo, parentShell);
+	}
+	public void setIsAutoClose(boolean isAutoClose){
+		this.isAutoClose = isAutoClose;
+	}
+	public void setIsMove(boolean isMove){
+		this.isMove = isMove;
+	}
+	public void setMoveSpeed(int moveSpeed){
+		this.moveSpeed = moveSpeed;
+	}
+	private void setShellPosition(ShellPositionEnum position){
+		int screenH = Toolkit.getDefaultToolkit().getScreenSize().height;
+	    int screenW = Toolkit.getDefaultToolkit().getScreenSize().width;
+	    Point size = shell.getSize();
+	    switch (position) {	
+	    case TOP_LEFT:
+	    	break;
+		case TOP_MID:
+			shellLocationx = screenW/2;
+			break;
+		case TOP_RIGHT:
+			shellLocationx = screenW - size.x;
+		break;
+		case MID_LEFT:
+			shellLocationy = screenH/2;
+		break;
+		case MID_MID:
+			shellLocationx = screenW/2;
+			shellLocationy = screenH/2;
+		break;
+		case MID_RIGTH:
+			shellLocationx = screenW - size.x;
+			shellLocationy = screenH/2;
+		break;
+		case BOT_LEFT:
+			shellLocationy = screenH - size.y;
+		break;
+		case BOT_MID:
+			shellLocationx = screenW/2;
+			shellLocationy = screenH - size.y;
+		break;
+		case BOT_RIGTH:
+			shellLocationx = screenW - size.x;
+			shellLocationy = screenH - size.y;
+		break;
+		default:
+			break;
+	    }
+	}
+	public static AlertMessageDialog getInstance(){
+		if(_instance == null){
+			_instance = new AlertMessageDialog();
+		}
+		return _instance;
+	}
+	
+	public void showAlert(){
+		shell.setBackground(shellBackColor);
+		shell.setAlpha(alpha);
+		shell.setSize(this.shellWidth,this.shellHeight);
+		shell.setBackgroundMode(SWT.INHERIT_FORCE); 
+		if(shellLayout != null){
+			shell.setLayout(shellLayout);
+		}
+		Composite parentComposite = new Composite(shell,SWT.NONE);
+		float height = this.labelFont.getFontData()[0].height;
+		float labelLength = 1.5f*this.alertInfo.length()*height;
+		parentComposite.setSize((int)labelLength, 2*(int)height);
+		parentComposite.setLocation(10, 0);
+		this.fisrtLabel = new Label(parentComposite,SWT.NONE);
+		fisrtLabel.setBackground(labelBackColor);
+		fisrtLabel.setForeground(labelColor);
+		fisrtLabel.setFont(labelFont);
+		fisrtLabel.setText(this.alertInfo);
+		fisrtLabel.setLocation(0,0);
+//		fisrtLabel.pack();
+		
+		
+		fisrtLabel.setSize((int)labelLength, 2*(int)height);
+		if(isMove){
+			this.secondLabel = new Label(parentComposite,SWT.NONE);
+			secondLabel.setBackground(labelBackColor);
+//			secondLabel.setForeground(labelColor);
+			secondLabel.setFont(labelFont);
+			secondLabel.setText(this.alertInfo);
+			secondLabel.setLocation(fisrtLabel.getSize().x,0);
+			secondLabel.setSize((int)labelLength, 2*(int)height);
+		}
+		if(this.position != null){
+			setShellPosition(position);
+		}
+		shell.setLocation(shellLocationx, shellLocationy);
+		shell.open();
+		if(this.isMove){
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					autoMove();
+					
+				}
+			}).start();
+		}
+		if(this.isAutoClose){
+	    	Timer timer = new Timer();
+	    	timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					autoCloseAlert();
+				}
+				}, this.closeSeconds*1000);
+		    
+		}
+//		 shell.setSize(15*str.length(),hello.getBounds().height+2);
+		
+	}
+	public void setLabelBackGround(Color labelBackColor){
+		this.labelBackColor = labelBackColor; 
+	}
+	public void setLabelFontColor(Color labelColor){
+		this.labelColor = labelColor; 
+	}
+	public void setLabelFont(Font labelFont){
+		this.labelFont = labelFont;
+	}
+	public void setShellLayout(Layout shellLayout){
+		this.shellLayout = shellLayout;
+	}
+	public void setAlpha(int alpha){
+		this.alpha = alpha;
+	}
+	public void closeAlert(){
+		if(this.fisrtLabel != null && !fisrtLabel.isDisposed()){
+			fisrtLabel.isDisposed();
+			fisrtLabel = null;
+		}
+		if(this.secondLabel != null && !secondLabel.isDisposed()){
+			secondLabel.isDisposed();
+			secondLabel = null;
+		}
+		if(shell != null && !shell.isDisposed()){
+			shell.close();
+			shell.dispose();
+			shell = null;
+		}
+	}
+		private void autoCloseAlert(){
+			Display.getDefault().syncExec(new Runnable() {
+	    	    public void run() {
+			if(shell != null && !shell.isDisposed()){
+				shell.close();
+				shell.dispose();
+				shell = null;
+			}
+			  }
+		});
+	}
+	public void setCloseSeconds(int closeSeconds){
+		this.closeSeconds = closeSeconds;
+	} 
+	private void autoMove(){
+		while (true) {
+			try {
+				Thread.sleep(500);
+				Display.getDefault().asyncExec(new Runnable() {
+					@Override
+					public void run() {
+						if(fisrtLabel != null &&!fisrtLabel.isDisposed()){
+							Point firstLocation = fisrtLabel.getLocation();
+							Point secondLocation = secondLabel.getLocation();
+							if(firstLocation.x < secondLocation.x){
+								if(firstLocation.x+fisrtLabel.getSize().x <= 0){
+									fisrtLabel.setLocation(secondLocation.x+secondLabel.getSize().x, 0);
+								}
+							}else{
+								if(secondLocation.x+secondLabel.getSize().x <= 0){
+									secondLabel.setLocation(firstLocation.x+fisrtLabel.getSize().x, 0);
+								}
+							}
+							fisrtLabel.setLocation(fisrtLabel.getLocation().x-moveSpeed,fisrtLabel.getLocation().y);
+							secondLabel.setLocation(secondLabel.getLocation().x-moveSpeed,secondLabel.getLocation().y);
+						}else{
+							return;
+						}
+						
+					}
+				});
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+}
--- daliy_test/src/main/java/com/fantasybaby/rcp/TestShell.java
+++ daliy_test/src/main/java/com/fantasybaby/rcp/TestShell.java
@@ -1,14 +1,42 @@
 package com.fantasybaby.rcp;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.internal.win32.OS;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 public class TestShell {
-	public static void main(String[] args) throws InterruptedException {   
+	public static void main(String[] args) throws InterruptedException {
+		
+		Display display = new Display();
+	    Shell shell=new Shell(display);//shell是程序的主窗体 
+
+	    shell.setLayout(null);         //设置shell的布局方式 
+
+	    shell.setText("Java应用程序");  //设置主窗体的标题 
+
+	    shell.setSize(1000,500);        //设置主窗体的大小 
+	    AlertMessageDialog am =  AlertMessageDialog.getInstance();
+	    am.init("有拣货任务分配在正在（排队）补货的货架上，订单已经超时，请立即完成补货操作以释放货架！！", shell, 800, 28, 200,20);
+	    am.setAlpha(150);
+	    am.setIsMove(true);
+	    am.setMoveSpeed(20);
+//	    am.setIsAutoClose(true);
+//	    am.setCloseSeconds(10);
+	    shell.open();
+	    am.showAlert();
+	    while(!shell.isDisposed()){  //如果主窗体没有关闭则一直循环 
+
+	        if(!display.readAndDispatch()){  //如果display不忙 
+
+	        display.sleep();    //休眠 
+
+	        } 
+
+	    } 
+	    display.dispose();  
+		/*   
 	Display display = new Display();
     Shell shell=new Shell(display);//shell是程序的主窗体 
 
@@ -22,7 +50,7 @@ public class TestShell {
 
     shell.setBackground(SWTResourceManager.getColor(SWT.COLOR_DARK_GREEN));   //设置窗体的背景颜色
     Shell shell2 = new Shell(shell,SWT.BORDER_SOLID);
-    shell2.setLayout(new FillLayout());
+//    shell2.setLayout(new FillLayout());
     shell2.setBackground(SWTResourceManager.getColor(SWT.COLOR_TITLE_BACKGROUND));
     shell2.setAlpha(150);
     shell2.setBackgroundMode(SWT.INHERIT_FORCE); 
@@ -31,12 +59,22 @@ public class TestShell {
 //    OS.SetWindowLong(hello.handle,OS.GWL_EXSTYLE,OS.GetWindowLong(hello.handle, OS.GWL_EXSTYLE)^0x80000);
     hello.setBackground(SWTResourceManager.getColor(SWT.COLOR_RED));
     hello.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", StationUIUtils.scale(15), SWT.BOLD));
-    hello.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
-    
-    hello.setText("有拣货任务分配在正在（排队）补货的货架上，订单已经超时，请立即完成补货操作以释放货架！！");//设置文本框信息 
-    hello.pack();    //自动调整文本框的大小
+//    hello.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+    String str = "有拣货任务分配在正在（排队）补货的货架上，订单已经超时，请立即完成补货操作以释放货架！！";
+    hello.setText(str);//设置文本框信息 
+//    hello.pack();    //自动调整文本框的大小
+    hello.setSize(1000, 28);
     hello.setLocation(0,0);
-    shell2.setSize(400,hello.getBounds().height+2);
+    
+    final Label hello1=new Label(shell2,SWT.NONE); //声明一个可以显示多行信息的文本框
+    hello1.setBackground(SWTResourceManager.getColor(SWT.COLOR_RED));
+    hello1.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", StationUIUtils.scale(15), SWT.BOLD));
+	//  hello.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+	  hello1.setText("aaaa"+str);//设置文本框信息 
+	//  hello.pack();    //自动调整文本框的大小
+	  hello1.setSize(1000, 28);
+	  hello1.setLocation(hello.getSize().x,0);
+    shell2.setSize(15*str.length(),hello.getBounds().height+2);
 //    shell2.pack();  //自动调整主窗体的大小 
     shell.open();    //打开主窗体 
     shell2.open();
@@ -50,13 +88,29 @@ public class TestShell {
 		public void run() {
 
 			 for (int i = 0; i < width; i++) {
-
 					try {
 						Thread.sleep(500);
 						Display.getDefault().asyncExec(new Runnable() {
 							@Override
 							public void run() {
-								hello.setLocation(hello.getLocation().x-3,hello.getLocation().y);
+								if(hello != null &&!hello.isDisposed()){
+									Point firstLocation = hello.getLocation();
+									Point secondLocation = hello1.getLocation();
+									if(firstLocation.x < secondLocation.x){
+										if(firstLocation.x+hello.getSize().x <= 0){
+											hello.setLocation(secondLocation.x+hello1.getSize().x, 0);
+										}
+									}else{
+										if(secondLocation.x+hello1.getSize().x <= 0){
+											hello1.setLocation(firstLocation.x+hello.getSize().x, 0);
+										}
+									}
+									hello.setLocation(hello.getLocation().x-10,hello.getLocation().y);
+									hello1.setLocation(hello1.getLocation().x-10,hello.getLocation().y);
+								}else{
+									return;
+								}
+								
 							}
 						});
 					} catch (InterruptedException e) {
@@ -80,7 +134,8 @@ public class TestShell {
     } 
 
     display.dispose();      //销毁display 
-    }
+    */
+	}
 	public void runTheWords(){
 		
 	}
