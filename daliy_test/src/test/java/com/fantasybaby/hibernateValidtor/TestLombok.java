901a8791c1a0264d52235fd03a3065d6b9b51bf2
FantasyBaby <625353861@qq.com>
2017-06-05 16:40:53 +0800

    add lombok test

---
+++ daliy_test/src/test/java/com/fantasybaby/hibernateValidtor/TestLombok.java
@@ -0,0 +1,20 @@
+package com.fantasybaby.hibernateValidtor;
+
+import com.fantasybaby.lombok.UserBean;
+import org.junit.Test;
+
+/**
+ * Created by fanta on 2017-06-05.
+ */
+public class TestLombok {
+    @Test
+    public void testLombokMethod(){
+        UserBean bean = new UserBean();
+        bean.setUserName("呵呵");
+        bean.setAge(13);
+
+        System.out.println(bean.getAge()+":"+bean.getUserName());
+
+
+    }
+}
