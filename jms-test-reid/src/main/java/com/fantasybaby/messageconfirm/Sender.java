85fceed1c29b9a1ce901f7652254756841af094b
FantasyBaby <625353861@qq.com>
2017-07-02 22:04:29 +0800

    add test client_acknowlege

---
+++ jms-test-reid/src/main/java/com/fantasybaby/messageconfirm/Sender.java
@@ -0,0 +1,62 @@
+package com.fantasybaby.messageconfirm;
+
+import com.fantasybaby.p2p.QBorrower;
+import com.fantasybaby.util.UserNameUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+
+/**
+ * 这个类用于发送消息
+ * 但是消息并非一次发送完成
+ */
+public class Sender {
+    private QueueConnection queueConnection;
+    private QueueSession queueSession;
+    private Queue requestQueue;
+    private Queue responseQueue;
+    private static Logger _logger ;
+    static {
+        _logger = LoggerFactory.getLogger(QBorrower.class);
+    }
+    public Sender(String queueFactory,String requstQueueName){
+        try {
+            InitialContext context = new InitialContext();
+            QueueConnectionFactory connectionFactory = (QueueConnectionFactory) context.lookup(queueFactory);
+            queueConnection = connectionFactory.createQueueConnection(UserNameUtil.USERNAME,UserNameUtil.USERPWD);
+
+            queueSession = queueConnection.createQueueSession(false,Session.CLIENT_ACKNOWLEDGE);
+            requestQueue = (Queue) context.lookup(requstQueueName);
+            queueConnection.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void sendGroup() throws JMSException {
+        sendMessage("1","st");
+        sendMessage("2","");
+        sendMessage("3","");
+        sendMessage("4","");
+        sendMessage("5","en");
+        this.queueConnection.close();
+    }
+    private void sendMessage(String messageStr,String flag){
+        try {
+            TextMessage message = queueSession.createTextMessage(messageStr);
+            message.setStringProperty("marker",flag);
+            QueueSender sender = queueSession.createSender(requestQueue);
+            sender.send(message);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Sender lender = new Sender("QueueConnectionFactory", "requestQueue");
+        System.out.println("please input message ---------------------");
+        lender.sendGroup();
+        System.out.println("end message ---------------------");
+
+    }
+}
