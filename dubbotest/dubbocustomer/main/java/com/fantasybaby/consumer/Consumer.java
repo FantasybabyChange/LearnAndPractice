407614f62bccb392a9b2f4f0a5b9089117a96d20
fantasybaby <625353861@qq.com>
2017-11-16 21:04:46 +0800

    add dubbole test

--- dubbotest/dubbocustomer/main/java/com/fantasybaby/consumer/Consumer.java
+++ dubbotest/dubbocustomer/main/java/com/fantasybaby/consumer/Consumer.java
@@ -1,15 +1,27 @@
 package com.fantasybaby.consumer;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.fantasybaby.service.IDubboService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component(value = "consumer")
 public class Consumer {
+    @Reference(version = "1.0.0")
+    private IDubboService dubboService;
+
+    public String  sayHello(){
+        return dubboService.getName();
+    }
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[]{"customer.xml"});
         context.start();
-        IDubboService demoService = (IDubboService) context.getBean("demoService"); // obtain proxy object for remote invocation
+        Consumer consumer = (Consumer) context.getBean("consumer");
+        System.out.println(consumer.sayHello());
+        /*IDubboService demoService = (IDubboService) context.getBean("demoService"); // obtain proxy object for remote invocation
         String hello = demoService.getName(); // execute remote invocation
-        System.out.println("consumer" + hello+ "----"); // show the result
+        System.out.println("consumer" + hello+ "----"); // show the result*/
     }
 }
--- dubbotest/dubboprovider/main/java/com/fantasybaby/provider/impl/DubboProvider.java
+++ dubbotest/dubboprovider/main/java/com/fantasybaby/provider/impl/DubboProvider.java
@@ -1,7 +1,8 @@
 package com.fantasybaby.provider.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.fantasybaby.service.IDubboService;
-
+@Service(version = "1.0.0")
 public class DubboProvider implements IDubboService {
     public String getName() {
         return "=== DubboProvider Name ===";
