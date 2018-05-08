732ca8ef01e36daf66a174fcf614b43dc0619bbb
fantasybaby <625353861@qq.com>
2018-05-08 18:24:17 +0800

    add groovy test

--- groovy-test/src/main/groovy/FileIO.groovy
+++ groovy-test/src/main/groovy/FileIO.groovy
@@ -4,3 +4,12 @@
  * @author liuxi
  * @date 2018-05-05
  */
+// map
+def map = ['key1':'value','key2':'value2']
+println map.getClass()
+map.each {
+    entry ->
+        println entry.key+"--"+entry.value
+
+}
+//list
