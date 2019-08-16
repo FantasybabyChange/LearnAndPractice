4020a75507337d2c124b2ad6f736bf247e3bfc69
fantasybaby <625353861@qq.com>
2019-08-16 11:32:23 +0800

    better allocator

--- concurrent-test/src/main/java/com/fantasybaby/basic/deadlock/FixDeadlockUseAccount.java
+++ concurrent-test/src/main/java/com/fantasybaby/basic/deadlock/FixDeadlockUseAccount.java
@@ -32,30 +32,4 @@ public class FixDeadlockUseAccount {
         }
     }
 }
-class Allocator {
-    private static Allocator allocator = new Allocator();
-    private List<Object> als =new ArrayList<>();
-    private Allocator(){
-    }
-    public static Allocator getInstance(){
-        return allocator;
-    }
-    // 一次性申请所有资源
-    synchronized boolean apply(
-            Object from, Object to){
-        if(als.contains(from) ||
-                als.contains(to)){
-            return false;
-        } else {
-            als.add(from);
-            als.add(to);
-        }
-        return true;
-    }
-    // 归还资源
-    synchronized void free(
-            Object from, Object to){
-        als.remove(from);
-        als.remove(to);
-    }
-}
+
