0f74a07cabb3fcadf4180a98355f7ad5d3167c67
fantasybaby <625353861@qq.com>
2019-11-14 09:51:48 +0800

    add multiverse

---
+++ concurrent-test/src/main/java/com/fantasybaby/basic/stm/SafeAccount.java
@@ -0,0 +1,29 @@
+package com.fantasybaby.basic.stm;
+
+import org.multiverse.api.StmUtils;
+import org.multiverse.api.references.TxnLong;
+import static org.multiverse.api.StmUtils.*;
+
+/**
+ * @author: liuxi
+ * @time: 2019/11/13 20:55
+ */
+public class SafeAccount {
+
+        //余额
+        private TxnLong balance;
+        //构造函数
+        public SafeAccount(long balance){
+            this.balance = StmUtils.newTxnLong(balance);
+        }
+        //转账
+        public void transfer(SafeAccount to, int amt){
+            //原子化操作
+            atomic(()->{
+                if (this.balance.get() > amt) {
+                    this.balance.decrement(amt);
+                    to.balance.increment(amt);
+                }
+            });
+        }
+}
