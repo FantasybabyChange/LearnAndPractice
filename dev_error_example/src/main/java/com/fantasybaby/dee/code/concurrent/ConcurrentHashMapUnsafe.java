2f9f6420a5247cd60a78f41f04c83536f3221ecc
fantasybaby <625353861@qq.com>
2020-10-12 23:41:43 +0800

    add test

--- dev_error_example/src/main/java/com/fantasybaby/dee/code/concurrent/ConcurrentHashMapUnsafe.java
+++ dev_error_example/src/main/java/com/fantasybaby/dee/code/concurrent/ConcurrentHashMapUnsafe.java
@@ -3,6 +3,14 @@ package com.fantasybaby.dee.code.concurrent;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * concurrentHashMap not safe
@@ -11,5 +19,45 @@ import java.util.Map;
  */
 public class ConcurrentHashMapUnsafe {
     private static Map<String,Integer> hashMap = Maps.newConcurrentMap();
-//    Maps
+
+    //线程个数
+    private static int THREAD_COUNT = 10;
+    //总元素数量
+    private static int ITEM_COUNT = 1000;
+
+    /**帮助方法，用来获得一个指定元素数量模拟数据的ConcurrentHashMap
+     *
+     * @param count
+     * @return
+     */
+    private ConcurrentHashMap<String, Long> getData(int count) {
+        return LongStream.rangeClosed(1, count)
+                .boxed()
+                .collect(Collectors.toConcurrentMap(i -> UUID.randomUUID().toString(), Function.identity(),
+                        (o1, o2) -> o1, ConcurrentHashMap::new));
+    }
+
+    public String wrong() throws InterruptedException {
+        ConcurrentHashMap<String, Long> concurrentHashMap = getData(ITEM_COUNT - 100);
+        //初始900个元素
+        System.out.println(String.format("init size:%s", concurrentHashMap.size()));
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+        //使用线程池并发处理逻辑
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
+            //查询还需要补充多少个元素
+            int gap = ITEM_COUNT - concurrentHashMap.size();
+            System.out.println(String.format("gap size:%s-%s",Thread.currentThread().getName(), gap));
+            //补充元素
+            concurrentHashMap.putAll(getData(gap));
+        }));
+        //等待所有任务完成
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        //最后元素个数会是1000吗？
+        System.out.println(String.format("finish size:{}", concurrentHashMap.size()));
+
+        return "OK";
+    }
+
 }
--- dev_error_example/src/test/java/com/fantasybaby/dee/concurrent/ConcurrentHashMapTest.java
+++ dev_error_example/src/test/java/com/fantasybaby/dee/concurrent/ConcurrentHashMapTest.java
@@ -1,11 +1,22 @@
 package com.fantasybaby.dee.concurrent;
 
+import com.fantasybaby.dee.code.concurrent.ConcurrentHashMapUnsafe;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConcurrentHashMapTest {
+    ConcurrentHashMapUnsafe map;
+    @Before
+    public void init(){
+        map = new ConcurrentHashMapUnsafe();
+    }
     @Test
-    public void testConcurrentHashMap(){
-        System.out.println("hello");
+    public void testWrong(){
+        try {
+            map.wrong();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
