4b42887e735294f2397a53f0b4a159c1178c30b1
Reid Liu <Reid.liu@kuka.com>
2020-12-03 17:20:23 +0800

    change Product

--- jdk-8-learn/src/main/java/com/fantasybaby/study/java8test/domain/Product.java
+++ jdk-8-learn/src/main/java/com/fantasybaby/study/java8test/domain/Product.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private Long id;
     private String name;
@@ -16,16 +17,16 @@ public class Product {
 
 
     public static List<Product> getData() {
-//        return Arrays.asList(
-//                new Product(1L, "苹果", 1.0),
-//                new Product(2L, "桔子", 2.0),
-//                new Product(3L, "香蕉", 3.0),
-//                new Product(4L, "芒果", 4.0),
-//                new Product(5L, "西瓜", 5.0),
-//                new Product(6L, "葡萄", 6.0),
-//                new Product(7L, "桃子", 7.0),
-//                new Product(8L, "椰子", 8.0),
-//                new Product(9L, "菠萝", 9.0),
-//                new Product(10L, "石榴", 10.0));
+        return Arrays.asList(
+                new Product(1L, "苹果", 1.0),
+                new Product(2L, "桔子", 2.0),
+                new Product(3L, "香蕉", 3.0),
+                new Product(4L, "芒果", 4.0),
+                new Product(5L, "西瓜", 5.0),
+                new Product(6L, "葡萄", 6.0),
+                new Product(7L, "桃子", 7.0),
+                new Product(8L, "椰子", 8.0),
+                new Product(9L, "菠萝", 9.0),
+                new Product(10L, "石榴", 10.0));
     }
 }
