20171893f1ecb80b59778d7854a7ab689e6e0a2b
fantasybaby <625353861@qq.com>
2020-12-09 23:39:18 +0800

    add group by

--- jdk-8-learn/src/main/java/com/fantasybaby/study/java8test/lambadatest/CollectorTest.java
+++ jdk-8-learn/src/main/java/com/fantasybaby/study/java8test/lambadatest/CollectorTest.java
@@ -3,10 +3,10 @@ package com.fantasybaby.study.java8test.lambadatest;
 import com.fantasybaby.study.java8test.domain.Order;
 import com.fantasybaby.study.java8test.domain.OrderItem;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
+import static java.util.Comparator.comparingDouble;
 import static java.util.stream.Collectors.*;
 
 public class CollectorTest {
@@ -50,5 +50,55 @@ public class CollectorTest {
         System.out.println(orders.stream().collect(averagingInt(order ->
                 order.getOrderItemList().stream()
                         .collect(summingInt(OrderItem::getProductQuantity)))));
+    }
+
+    public void collectorGroupBy() {
+        //按照用户名分组，统计下单数量
+        System.out.println(orders.stream().collect(groupingBy(Order::getCustomerName, counting()))
+                .entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).collect(toList()));
+
+        //按照用户名分组，统计订单总金额
+        System.out.println(orders.stream().collect(groupingBy(Order::getCustomerName, summingDouble(Order::getTotalPrice)))
+                .entrySet().stream().sorted(Map.Entry.<String, Double>comparingByValue().reversed()).collect(toList()));
+
+        //按照用户名分组，统计商品采购数量
+        System.out.println(orders.stream().collect(groupingBy(Order::getCustomerName,
+                summingInt(order -> order.getOrderItemList().stream()
+                        .collect(summingInt(OrderItem::getProductQuantity)))))
+                .entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).collect(toList()));
+
+        //统计最受欢迎的商品，倒序后取第一个
+        orders.stream()
+                .flatMap(order -> order.getOrderItemList().stream())
+                .collect(groupingBy(OrderItem::getProductName, summingInt(OrderItem::getProductQuantity)))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        //统计最受欢迎的商品的另一种方式，直接利用maxBy
+        orders.stream()
+                .flatMap(order -> order.getOrderItemList().stream())
+                .collect(groupingBy(OrderItem::getProductName, summingInt(OrderItem::getProductQuantity)))
+                .entrySet().stream()
+                .collect(maxBy(Map.Entry.comparingByValue()))
+                .map(Map.Entry::getKey)
+                .ifPresent(System.out::println);
+
+        //按照用户名分组，选用户下的总金额最大的订单
+        orders.stream().collect(groupingBy(Order::getCustomerName, collectingAndThen(maxBy(comparingDouble(Order::getTotalPrice)), Optional::get)))
+                .forEach((k, v) -> System.out.println(k + "#" + v.getTotalPrice() + "@" + v.getPlacedAt()));
+
+        //根据下单年月分组，统计订单ID列表
+        System.out.println(orders.stream().collect
+                (groupingBy(order -> order.getPlacedAt().format(DateTimeFormatter.ofPattern("yyyyMM")),
+                        mapping(order -> order.getId(), toList()))));
+
+    //根据下单年月+用户名两次分组，统计订单ID列表
+        System.out.println(orders.stream().collect
+                (groupingBy(order -> order.getPlacedAt().format(DateTimeFormatter.ofPattern("yyyyMM")),
+                        groupingBy(order -> order.getCustomerName(),
+                                mapping(order -> order.getId(), toList())))));
     }
 }
