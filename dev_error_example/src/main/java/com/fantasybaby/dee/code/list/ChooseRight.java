35d3d97dd3dcd67ffd06e34f0691c78b4fb53c64
Reid Liu <Reid.liu@kuka.com>
2021-01-29 15:16:24 +0800

    add choose list

--- dev_error_example/src/main/java/com/fantasybaby/dee/code/list/ChooseRight.java
+++ dev_error_example/src/main/java/com/fantasybaby/dee/code/list/ChooseRight.java
@@ -1,12 +1,18 @@
 package com.fantasybaby.dee.code.list;
 
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.Assert;
+import org.springframework.util.StopWatch;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -22,7 +28,7 @@ public class ChooseRight {
         private int orderId;
     }
 
-    private static Object listSearch(int elementCount, int loopCount) {
+    public Object listSearch(int elementCount, int loopCount) {
         List<Order> list = IntStream.rangeClosed(1, elementCount).mapToObj(i -> new Order(i)).collect(Collectors.toList());
         IntStream.rangeClosed(1, loopCount).forEach(i -> {
             int search = ThreadLocalRandom.current().nextInt(elementCount);
@@ -31,5 +37,38 @@ public class ChooseRight {
 
         });
         return list;
+    }
+
+    /**
+     * 牺牲空间换时间
+     * @param elementCount
+     * @param loopCount
+     * @return
+     */
+    public Object mapSearch(int elementCount, int loopCount) {
+        Map<Integer, Order> map = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toMap(Function.identity(), i -> new Order(i)));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> {
+            int search = ThreadLocalRandom.current().nextInt(elementCount);
+            Order result = map.get(search);
+            Assert.assertTrue(result != null && result.getOrderId() == search);
+        });
+        return map;
+    }
+
+    public static void main(String[] args) {
+//        System.setProperty("java.vm.name","Java HotSpot(TM) ");
+        ChooseRight cr = new ChooseRight();
+        int elementCount = 1000000;
+        int loopCount = 1000;
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("listSearch");
+        Object list = cr.listSearch(elementCount, loopCount);
+        System.out.println(ObjectSizeCalculator.getObjectSize(list));
+        stopWatch.stop();
+        stopWatch.start("mapSearch");
+        Object map = cr.mapSearch(elementCount, loopCount);
+        stopWatch.stop();
+        System.out.println(ObjectSizeCalculator.getObjectSize(map));
+        System.out.println(stopWatch.prettyPrint());
     }
 }
