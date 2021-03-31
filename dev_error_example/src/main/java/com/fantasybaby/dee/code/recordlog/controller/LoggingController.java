ed9c4c03002476fcb5ab33e1b938c40c6bac60fe
Reid Liu <Reid.liu@kuka.com>
2021-03-31 20:06:47 +0800

    add async log

--- dev_error_example/src/main/java/com/fantasybaby/dee/code/recordlog/controller/LoggingController.java
+++ dev_error_example/src/main/java/com/fantasybaby/dee/code/recordlog/controller/LoggingController.java
@@ -1,9 +1,16 @@
 package com.fantasybaby.dee.code.recordlog.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author fantasybaby
@@ -19,5 +26,25 @@ public class LoggingController {
         log.info("info");
         log.warn("warn");
         log.error("error");
+    }
+
+
+    @GetMapping("performance")
+    public void performance(@RequestParam(name = "count", defaultValue = "1000") int count) {
+        long begin = System.currentTimeMillis();
+        String payload = IntStream.rangeClosed(1, 1000000)
+                .mapToObj(__ -> "a")
+                .collect(Collectors.joining("")) + UUID.randomUUID().toString();
+        IntStream.rangeClosed(1, count).forEach(i -> log.info("{} {}", i, payload));
+        Marker timeMarker = MarkerFactory.getMarker("time");
+        log.info(timeMarker, "took {} ms", System.currentTimeMillis() - begin);
+    }
+
+
+    @GetMapping("manylog")
+    public void manylog(@RequestParam(name = "count", defaultValue = "1000") int count) {
+        long begin = System.currentTimeMillis();
+        IntStream.rangeClosed(1, count).forEach(i -> log.info("log-{}", i));
+        System.out.println("took " + (System.currentTimeMillis() - begin) + " ms");
     }
 }
