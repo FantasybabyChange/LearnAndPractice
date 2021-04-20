b3bfd9303845f7b61c1d8b2ca61a78cb5efdf5ea
Reid Liu <Reid.liu@kuka.com>
2021-04-20 15:40:36 +0800

    add all line

--- dev_error_example/src/main/java/com/fantasybaby/dee/code/fileio/EncodeFile.java
+++ dev_error_example/src/main/java/com/fantasybaby/dee/code/fileio/EncodeFile.java
@@ -3,8 +3,10 @@ package com.fantasybaby.dee.code.fileio;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Hex;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -23,7 +25,12 @@ public class EncodeFile {
         log.info("bytes:{}", Hex.encodeHexString(Files.readAllBytes(Paths.get("hello.txt"))).toUpperCase());
     }
 
-    public static void readGBKFile() throws IOException {
+    /**
+     * 默认使用UTF-8
+     *
+     * @throws IOException
+     */
+    public static void readGBKFileNoCharset() throws IOException {
 
         char[] chars = new char[10];
         String content = "";
@@ -36,9 +43,34 @@ public class EncodeFile {
         log.info("result:{}", content);
     }
 
+
+    private static void readGBKFileCharset() throws IOException {
+        char[] chars = new char[10];
+        String content = "";
+        try (FileInputStream fileInputStream = new FileInputStream("hello.txt");
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, Charset.forName("GBK"))) {
+            int count;
+            while ((count = inputStreamReader.read(chars)) != -1) {
+                content += new String(chars, 0, count);
+            }
+        }
+        log.info("result: {}", content);
+    }
+
+    /**
+     * read all line
+     * 会OOM
+     * @throws IOException
+     */
+    private static void readGBKFileAllLineCharset() throws IOException {
+        log.info("result: {}", Files.readAllLines(Paths.get("hello.txt"), Charset.forName("GBK")).stream().findFirst().orElse(""));
+    }
+
     public static void main(String[] args) throws IOException {
         writeGBKFile();
-        readGBKFile();
+        readGBKFileNoCharset();
+        readGBKFileCharset();
+        readGBKFileAllLineCharset();
     }
 
 }
